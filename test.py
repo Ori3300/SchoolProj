4ed02ac49c29e6b3c34a8b2dde178b37bd2b589b
Ori3300 <Ori33000@gmail.com>
2024-12-26 12:35:59 +0200

    add file
    add file

---
+++ test.py
@@ -0,0 +1,16 @@
+import plotly.express as px
+
+fig = px.scatter_mapbox(
+    lat=[37.7749],
+    lon=[-122.4194],
+    text=["Sample Location"],
+    zoom=12,
+    height=500
+)
+
+fig.update_layout(
+    mapbox_style="mapbox://styles/mapbox/streets-v11",
+    mapbox_accesstoken=MAPBOX_ACCESS_TOKEN
+)
+
+fig.show()
